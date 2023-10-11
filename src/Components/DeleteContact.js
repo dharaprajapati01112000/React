df1d666fea26333a3ec5654cb7501343aa5f5455
Dhara Prajapati <dharaprajapati01112000@gmail.com>
2023-10-11 23:09:09 +0530

    react crud first commit

--- src/Components/DeleteContact.js
+++ src/Components/DeleteContact.js
@@ -1,3 +1,4 @@
+//deletecontact.js
 
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
--- src/Components/EditContact.js
+++ src/Components/EditContact.js
@@ -1,4 +1,4 @@
-//editcontact.js
+//editcontact.js code
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
